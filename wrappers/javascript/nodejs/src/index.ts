01f7329ce337c5ce827cc1d57d9de937a6f5118b
Ariel Gentile <gentilester@gmail.com>
2022-07-06 19:47:05 -0300

    feat(nodejs): binding testing (TODO: move to specific test or demo scripts)
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- wrappers/javascript/nodejs/src/index.ts
+++ wrappers/javascript/nodejs/src/index.ts
@@ -3,7 +3,6 @@ import { registerIndyCredx } from 'indy-credx-shared'
 
 import { NodeJSIndyCredx } from './NodeJSIndyCredx'
 
-
 const run = () => {
   const credx = new NodeJSIndyCredx()
   // NOTE: this is only required when using the functionality from within shared
@@ -13,13 +12,12 @@ const run = () => {
   console.log(credx.version())
   console.log(credx.getCurrentError())
   console.log(credx.generateNonce())
-  //console.log(credx.createMasterSecret())
   const schemaObj = credx.createSchema({
     name: 'schema-1',
     originDid: '55GkHamhTU1ZbTbV2ab9DE',
     version: '1',
     sequenceNumber: 1,
-    attributeNames: ['attribute-1', 'attribute-2'],
+    attributeNames: ['attr-1', 'attr-2'],
   })
   console.log(`Create Schema error:  ${credx.getCurrentError()}`)
 
@@ -30,17 +28,129 @@ const run = () => {
   console.log(`schemaGetAttribute returns: ${schemaId}`)
   console.log(`schemaGetAttribute error:  ${credx.getCurrentError()}`)
 
-  const credDefObj = credx.createCredentialDefinition({
+  const [credDefObj, credDefPvt, keyProof] = credx.createCredentialDefinition({
     originDid: '55GkHamhTU1ZbTbV2ab9DE',
     schema: schemaObj,
     signatureType: 'CL',
-    supportRevocation: false,
+    supportRevocation: true,
     tag: 'TAG',
   })
   console.log(`Create Cred Def Error: ${credx.getCurrentError()}`)
 
   const encoded = credx.encodeCredentialAttributes({ key1: 'value2', key2: 'value1' })
   console.log(`encoded: ${JSON.stringify(encoded)}`)
+
+  const [revRegDef, revRegDefPrivate, revReg, revRegInitDelta] = credx.createRevocationRegistry({
+    originDid: '55GkHamhTU1ZbTbV2ab9DE',
+    credentialDefinition: credDefObj,
+    tag: 'default',
+    revocationRegistryType: 'CL_ACCUM',
+    maximumCredentialNumber: 100,
+  })
+
+  const tailsPath = credx.revocationRegistryDefinitionGetAttribute({
+    object: revRegDef,
+    name: 'tails_location',
+  })
+  console.log(`revocationRegistryDefinitionGetAttribute returns: ${tailsPath}`)
+
+  const masterSecret = credx.createMasterSecret()
+  const masterSecretId = 'master secret id'
+
+  const credOfferObj = credx.createCredentialOffer({
+    schemaId,
+    credentialDefinition: credDefObj,
+    keyProof,
+  })
+  console.log(`Create Cred Offer Error: ${credx.getCurrentError()}`)
+
+  const [credReq, credReqMetadata] = credx.createCredentialRequest({
+    proverDid: '55GkHamhTU1ZbTbV2ab9DE',
+    credentialDefinition: credDefObj,
+    masterSecret,
+    masterSecretId,
+    credentialOffer: credOfferObj,
+  })
+  console.log(`Create Cred Request Error: ${credx.getCurrentError()}`)
+
+  const issuerRevIndex = 1
+
+  const [cred, revRegUpdated, revDelta] = credx.createCredential({
+    credentialDefinition: credDefObj,
+    credentialDefinitionPrivate: credDefPvt,
+    credentialOffer: credOfferObj,
+    credentialRequest: credReq,
+    attributeRawValues: { 'attr-1': 'test' },
+    // TODO: CredentialRevocationConfig
+    attributeEncodedValues: undefined,
+    revocationConfiguration: {
+      registryDefinition: revRegDef,
+      registryDefinitionPrivate: revRegDefPrivate,
+      registry: revReg,
+      registryIndex: issuerRevIndex,
+      tailsPath: tailsPath,
+    },
+  })
+
+  console.log(`Create Credential Error: ${credx.getCurrentError()}`)
+
+  const credReceived = credx.processCredential({
+    credential: cred,
+    credentialDefinition: credDefObj,
+    credentialRequestMetadata: credReqMetadata,
+    masterSecret,
+    revocationRegistryDefinition: revRegDef,
+  })
+
+  console.log(`Process credential error: ${credx.getCurrentError()}`)
+
+  const timestamp = new Date().getUTCDate()
+
+  const presRequestObj = credx.presentationRequestFromJson({
+    json: JSON.stringify({
+      name: 'proof',
+      version: '1.0',
+      nonce: '1234',
+      requested_attributes: {
+        reft: {
+          name: 'attr',
+          non_revoked: { from: timestamp, to: timestamp },
+        },
+      },
+      requested_predicates: {},
+      non_revoked: { from: timestamp, to: timestamp },
+      ver: '1.0',
+    }),
+  })
+
+  console.log(`Presentation Request from JSON error: ${credx.getCurrentError()}`)
+
+  console.log(`Presentation Request type name: ${credx.getTypeName({ object: presRequestObj })}`)
+  console.log(`Presentation Request type name: ${presRequestObj.typeName()}`)
+
+  const json = credx.getJson({ object: presRequestObj })
+
+  console.log(`Presentation Request JSON. Length ${json.length}: ${json}`)
+
+  const revRegIndex = credx.credentialGetAttribute({
+    object: cred,
+    name: 'rev_reg_index',
+  })
+
+  console.log(`credentialGetAttribute returns: ${revRegIndex}`)
+
+  const revocationRegistryIndex = revRegIndex === null ? 0 : parseInt(revRegIndex)
+
+  console.log(revocationRegistryIndex)
+  const revocationState = credx.createOrUpdateRevocationState({
+    revocationRegistryDefinition: revRegDef,
+    revocationRegistryDelta: revRegInitDelta,
+    revocationRegistryIndex,
+    timestamp,
+    tailsPath,
+  })
+
+  console.log(`Create Revocation State error: ${credx.getCurrentError()}`)
 }
 
 run()
