63d82d6e35e6d8c0d3c3a0827117ed592605bdc8
blu3beri <berend@animo.id>
2022-08-14 14:16:35 +0200

    feat(js): updated indycredx.h
    Signed-off-by: blu3beri <berend@animo.id>

--- wrappers/javascript/react-native/cpp/indyCredx.h
+++ wrappers/javascript/react-native/cpp/indyCredx.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <jsi/jsi.h>
-
 #include <include/libindy_credx.h>
 #include <turboModuleUtility.h>
 
@@ -10,5 +9,30 @@ using namespace facebook;
 namespace indyCredx {
 
 jsi::Value version(jsi::Runtime &rt, jsi::Object options);
+jsi::Value bufferFree(jsi::Runtime &rt, jsi::Object options);
+jsi::Value createCredential(jsi::Runtime &rt, jsi::Object options);
+jsi::Value createCredentialDefinition(jsi::Runtime &rt, jsi::Object options);
+jsi::Value createCredentialOffer(jsi::Runtime &rt, jsi::Object options);
+jsi::Value createCredentialRequest(jsi::Runtime &rt, jsi::Object options);
+jsi::Value createMasterSecret(jsi::Runtime &rt, jsi::Object options);
+jsi::Value createOrUpdateRevocationState(jsi::Runtime &rt, jsi::Object options);
+jsi::Value createPresentation(jsi::Runtime &rt, jsi::Object options);
+jsi::Value createRevocationRegistry(jsi::Runtime &rt, jsi::Object options);
+jsi::Value createSchema(jsi::Runtime &rt, jsi::Object options);
+jsi::Value credentialDefinitionGetAttribute(jsi::Runtime &rt, jsi::Object options);
+jsi::Value credentialGetAttribute(jsi::Runtime &rt, jsi::Object options);
+jsi::Value encodeCredentialAttributes(jsi::Runtime &rt, jsi::Object options);
+jsi::Value generateNonce(jsi::Runtime &rt, jsi::Object options);
+jsi::Value getCurrentError(jsi::Runtime &rt, jsi::Object options);
+jsi::Value mergeRevocationRegistryDeltas(jsi::Runtime &rt, jsi::Object options);
+jsi::Value objectGetJson(jsi::Runtime &rt, jsi::Object options);
+jsi::Value objectGetTypeName(jsi::Runtime &rt, jsi::Object options);
+jsi::Value processCredential(jsi::Runtime &rt, jsi::Object options);
+jsi::Value revocationRegistryDefinitionGetAttribute(jsi::Runtime &rt, jsi::Object options);
+jsi::Value revokeCredential(jsi::Runtime &rt, jsi::Object options);
+jsi::Value schemaGetAttribute(jsi::Runtime &rt, jsi::Object options);
+jsi::Value setDefaultLogger(jsi::Runtime &rt, jsi::Object options);
+jsi::Value verifyPresentation(jsi::Runtime &rt, jsi::Object options);
+jsi::Value objectFree(jsi::Runtime &rt, jsi::Object options);
 
 } // namespace indyCredx
